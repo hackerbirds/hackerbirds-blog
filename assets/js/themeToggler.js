c277c9b38f283d1e92a9872de7d3df20e2997596
hackerbirds <120066692+hackerbirds@users.noreply.github.com>
2023-08-21 15:02:22 +0000

    Remove theme button

--- assets/js/themeToggler.js
+++ assets/js/themeToggler.js
@@ -1,10 +1,4 @@
 function loadTheme() {
-	if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-		document.documentElement.classList.add("dark")
-	} else {
-		document.documentElement.classList.add("light")
-	}
-
 	if (localStorage.getItem('reader-mode')) {
 		document.documentElement.classList.add("reader")
 	}
@@ -15,21 +9,8 @@ function loadTheme() {
 
 	// load the buttons dynamically no that a no-JS user will not see the buttons 
 	// (they'd be useless to them)
-	document.getElementById("banner-id").insertAdjacentHTML("beforeend", "<button onclick=\"toggleTheme()\" name=\"Theme toggle button\" title=\"Toggle between light and dark theme\" class=\"banner-button theme-button\"></button>");
 	document.getElementById("banner-id").insertAdjacentHTML("beforeend", "<button onclick=\"toggleReader()\" name=\"Reader mode toggle button\" title=\"Toggle reader mode (increases contrast)\" class=\"banner-button reader-button\"></button>");
 	document.getElementById("banner-id").insertAdjacentHTML("beforeend", "<button onclick=\"toggleSerif()\" name=\"Seif mode toggle button\" title=\"Toggle serif mode (changes font)\" class=\"banner-button serif-button\"></button>");
-}
-
-function toggleTheme() {
-  if (localStorage.getItem('color-mode') === "dark") {
-  	document.documentElement.classList.remove("dark")
-  	document.documentElement.classList.add("light")
-  	localStorage.setItem("color-mode", "light")
-  } else {
-  	document.documentElement.classList.add("dark")
-  	document.documentElement.classList.remove("light")
-  	localStorage.setItem("color-mode", "dark")
-  }
 }
 
 function toggleReader() {
