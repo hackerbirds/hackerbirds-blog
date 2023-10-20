0afc715840605f4a92bc5f53af8dc60be0e4016a
hackerbirds <120066692+hackerbirds@users.noreply.github.com>
2023-10-20 11:40:57 +0000

    Readd theme toggle button + Banner button text changes

--- assets/js/themeToggler.js
+++ assets/js/themeToggler.js
@@ -1,16 +1,45 @@
 function loadTheme() {
+	if (localStorage.getItem('dark-theme')) {
+		if (localStorage.getItem('dark-theme') === "dark") {
+			document.documentElement.classList.add("dark")
+		} else {
+			document.documentElement.classList.add("light")
+		}
+	} else {
+		if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+			document.documentElement.classList.add("dark")
+			localStorage.setItem("dark-theme", "dark")
+		} else {
+			document.documentElement.classList.add("light")
+			localStorage.setItem("dark-theme", "light")
+		}
+	}
+
 	if (localStorage.getItem('reader-mode')) {
 		document.documentElement.classList.add("reader")
 	}
-	
+
 	if (localStorage.getItem('serif-mode')) {
 		document.documentElement.classList.add("serif")
 	}
 
-	// load the buttons dynamically no that a no-JS user will not see the buttons 
-	// (they'd be useless to them)
-	document.getElementById("banner-id").insertAdjacentHTML("beforeend", "<button onclick=\"toggleReader()\" name=\"Reader mode toggle button\" title=\"Toggle high contrast mode\" class=\"banner-button reader-button\"></button>");
-	document.getElementById("banner-id").insertAdjacentHTML("beforeend", "<button onclick=\"toggleSerif()\" name=\"Seif mode toggle button\" title=\"Toggle serif font mode\" class=\"banner-button serif-button\"></button>");
+	document.addEventListener("DOMContentLoaded", function () {
+		document.getElementById("banner-id").insertAdjacentHTML("beforeend", "<button onclick=\"toggleTheme()\" name=\"Toggle dark mode\" title=\"Toggle between light and dark theme\" class=\"banner-button dark-button\"></button>");
+		document.getElementById("banner-id").insertAdjacentHTML("beforeend", "<button onclick=\"toggleReader()\" name=\"Toggle high contrast mode\" title=\"Toggle high contrast mode\" class=\"banner-button reader-button\"></button>");
+		document.getElementById("banner-id").insertAdjacentHTML("beforeend", "<button onclick=\"toggleSerif()\" name=\"Toggle serif font mode\" title=\"Toggle serif font mode\" class=\"banner-button serif-button\"></button>");
+	});
+}
+
+function toggleTheme() {
+	if (localStorage.getItem('dark-theme') === "dark") {
+		document.documentElement.classList.remove("dark")
+		document.documentElement.classList.add("light")
+		localStorage.setItem("dark-theme", "light")
+	} else {
+		document.documentElement.classList.remove("light")
+		document.documentElement.classList.add("dark")
+		localStorage.setItem("dark-theme", "dark")
+	}
 }
 
 function toggleReader() {
@@ -24,7 +53,7 @@ function toggleReader() {
 }
 
 function toggleSerif() {
-  if (!localStorage.getItem('serif-mode')) {
+	if (!localStorage.getItem('serif-mode')) {
 		document.documentElement.classList.add("serif")
 		localStorage.setItem("serif-mode", "true")
 	} else {
